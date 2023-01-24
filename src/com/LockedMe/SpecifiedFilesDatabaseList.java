83847aeef5a4847cc1846ca9888ad48ff6ad9e6c
Mohamed Ishak <mohamed19.damir@gmail.com>
2023-01-24 16:19:32 +0200

    First Commit

---
+++ src/com/LockedMe/SpecifiedFilesDatabaseList.java
@@ -0,0 +1,60 @@
+package com.LockedMe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpecifiedFilesDatabaseList {
+	
+
+	private  List<SpecifiedFiles> files ;
+
+	
+	public SpecifiedFilesDatabaseList() {
+		   
+		files = new ArrayList<>();
+		
+	}
+	
+		 		
+     public  void addFile(SpecifiedFiles sp) {
+    	 files.add(sp);
+     }
+	
+	public List<SpecifiedFiles> displayAllFiles() {
+		
+		return files ;
+		}
+	
+	public boolean updateFiles(SpecifiedFiles sp) {
+		
+		for (int i = 1; i < files.size(); i++) {
+			
+			if(files.get(i).getId() == sp.getId()) {
+			files.set(i, sp);
+			return true ;
+			}
+				
+		}
+		return false;
+	}
+	
+	public boolean deleteFile(int id) {
+		
+		for (int i = 0; i < files.size(); i++) {
+			
+			if(files.get(i).getId() == id) {
+				files.remove(files.get(i));
+			    return true;
+			}
+		}
+		
+		return false ;
+	}
+	
+	public boolean addSpecifiedFile(SpecifiedFiles file) {
+		
+		
+		return false;
+	}
+
+}
---
+++ src/com/LockedMe/WelcomeScreen.java
@@ -0,0 +1,104 @@
+package com.LockedMe;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class WelcomeScreen {
+
+	
+	
+	public static void displayMenu() {
+
+		System.out.println();
+		System.err.println("  Please Select your choice");
+		System.out.println("1- Display All the Files");
+		System.out.println("2- Add New File ");
+		System.out.println("3- Search for a file");
+		System.out.println("4- Delete a specific File");
+		System.out.println("5- Navigate to the Main Screen");
+		System.out.println("6- Close The Application");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" WELCOME TO LOCKEDME APPLICATION  ");
+		System.out.println();
+		System.out.println("DEVELOPER : Mohamed Ishak , Test Engineer at VOIS");
+		
+
+		
+		//The options that will display to the user
+	    	
+		Scanner input = new Scanner(System.in);
+		SpecifiedFilesDatabaseList database = new SpecifiedFilesDatabaseList();
+		SpecifiedFiles sp = new SpecifiedFiles() ;
+		
+	
+		while(true) {
+			displayMenu();
+			int choice = input.nextInt();
+		switch (choice) {
+		case 1:
+			//Option 1
+			//Display all the files names in ascending order
+			//database.updateFiles(sp);
+			if(database.displayAllFiles().size() == 0)
+				System.out.println("No Files Are Added Yet");
+			
+				
+			for (SpecifiedFiles f : database.displayAllFiles()) {
+				System.out.println(f);
+			}
+			System.out.println();
+			break;
+			
+		case 2:
+			//Option 2
+			//Add , Delete and Search => a user specified file or Navigate to the main Context 
+		
+					System.out.println("Adding New file is selected");
+				//Adding files logic
+		
+			     System.out.println("Enter file name");
+                  String fname = input.next();
+                  System.out.println("Enter file Directory");
+                  String fDirectory= input.next() ;
+                  System.out.println("Enter File id");
+                  int fId = input.nextInt();
+                  Calendar cl ;
+                  
+                   
+                  sp = new SpecifiedFiles(fname,fDirectory,fId);
+                  database.addFile(sp);
+                  System.out.println("File is added successfully");
+                  
+                  
+				
+				
+	/*				System.out.println("Deleting a file is selected");
+				else if(selection == 3 )
+					System.out.println("Searching a file is selected");
+				else if
+					(selection == 4)
+					
+					System.out.println("Navigating to the Main Screen");
+			
+				
+			//1. Add New file to the list
+			//2. Delete a file in the list
+			//3. Search*/
+			break;
+			
+		case 3:
+			//Option 3
+			//Close the application
+			break;
+
+		default:
+			break;
+		}
+
+	}
+  }
+
+}

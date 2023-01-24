3c5fa05d7fe42b45b2cb4c5ccc2c0364d5625c62
Mohamed Ishak <mohamed19.damir@gmail.com>
2023-01-25 01:30:24 +0200

    Add Second Menu

--- src/com/LockedMe/SpecifiedFiles.java
+++ src/com/LockedMe/SpecifiedFiles.java
@@ -3,72 +3,58 @@ package com.LockedMe;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
-
 public class SpecifiedFiles {
-	
-	
-	String fileName ;
-	String dirName ;
-	private int id  ;
-	Calendar calendar ;
-	
-	public SpecifiedFiles() {}
-	
-	public SpecifiedFiles(String fileName , String dirName , int id) {
-		this.fileName = fileName ;
-		this.dirName = dirName ;
-		this.id = id ;
-	
-		
+
+	String fileName;
+	String dirName;
+	private int id;
+	Calendar calendar;
+
+	public SpecifiedFiles() {
 	}
-	
-     
-   
-		   public String getFileName() {
-			return fileName;
-		}
-				
-		
-		public void setFileName(String fileName) {
-			this.fileName = fileName;
-		}
 
-		public String getDirName() {
-			return dirName;
-		}
+	public SpecifiedFiles(String fileName, String dirName, int id) {
+		this.fileName = fileName;
+		this.dirName = dirName;
+		this.id = id;
 
-		public void setDirName(String dirName) {
-			this.dirName = dirName;
-		}
-		
-		
-		
-	
-    public int getId() {
-			return id;
-		}
+	}
 
-		public void setId(int id) {
-			this.id = id;
-		}
+	public String getFileName() {
+		return fileName;
+	}
 
-	public String getCurruntDate(){
-	calendar = Calendar.getInstance();
-	SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyy hh:mm:ss");
-	
-	return  date.format(calendar.getTime());
-   }
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getDirName() {
+		return dirName;
+	}
+
+	public void setDirName(String dirName) {
+		this.dirName = dirName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCurruntDate() {
+		calendar = Calendar.getInstance();
+		SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyy hh:mm:ss");
+
+		return date.format(calendar.getTime());
+	}
 
 	@Override
 	public String toString() {
-		return "SpecifiedFiles " +
-	            " fileName : " + fileName + 
-				" dirName : " + dirName + 
-				" id : " + id + 
-				" Calendar : " + getCurruntDate();
+		return "SpecifiedFiles [" + " fileName : " + fileName + ", dirName : " + dirName + ", id : " + id
+				+ ", Created Date : " + getCurruntDate() + "]";
 	}
-	
-	
-	
-  
+
 }
--- src/com/LockedMe/WelcomeScreen.java
+++ src/com/LockedMe/WelcomeScreen.java
@@ -1,97 +1,125 @@
 package com.LockedMe;
 
+import java.awt.Color;
 import java.util.Calendar;
 import java.util.Scanner;
 
 public class WelcomeScreen {
 
-	
-	
 	public static void displayMenu() {
 
 		System.out.println();
 		System.err.println("  Please Select your choice");
-		System.out.println("1- Display All the Files");
-		System.out.println("2- Add New File ");
-		System.out.println("3- Search for a file");
-		System.out.println("4- Delete a specific File");
-		System.out.println("5- Navigate to the Main Screen");
-		System.out.println("6- Close The Application");
+		System.out.println(" 1- Display All the Files");
+		System.out.println(" 2- Sup Menu options  { 1.Add - 2.Search - 3.Delete - Navigate }");
+		/*
+		 * System.out.println("   2.1- Add New File ");
+		 * System.out.println("   2.2- Search for a file");
+		 * System.out.println("   2.3- Delete a specific File");
+		 * System.out.println("   2.4- Navigate to the Main Screen");
+		 */
+		System.out.println(" 3- Close The Application");
 	}
+
+	public static void supMenuOptions() {
+		System.out.println("   2.1- Add New File ");
+		System.out.println("   2.2- Search for a file");
+		System.out.println("   2.3- Delete a specific File");
+		System.out.println("   2.4- Navigate to the Main Screen");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		System.out.println(" WELCOME TO LOCKEDME APPLICATION  ");
 		System.out.println();
 		System.out.println("DEVELOPER : Mohamed Ishak , Test Engineer at VOIS");
-		
 
-		
-		//The options that will display to the user
-	    	
+		// The options that will display to the user
+
 		Scanner input = new Scanner(System.in);
 		SpecifiedFilesDatabaseList database = new SpecifiedFilesDatabaseList();
-		SpecifiedFiles sp = new SpecifiedFiles() ;
-		
-	
-		while(true) {
+		SpecifiedFiles sp = new SpecifiedFiles();
+		int supOption = 0;
+
+		while (true) {
 			displayMenu();
 			int choice = input.nextInt();
-		switch (choice) {
-		case 1:
-			//Option 1
-			//Display all the files names in ascending order
-			//database.updateFiles(sp);
-			if(database.displayAllFiles().size() == 0)
-				System.out.println("No Files Are Added Yet");
-			
-				
-			for (SpecifiedFiles f : database.displayAllFiles()) {
-				System.out.println(f);
+
+			switch (choice) {
+			case 1:
+				// Option 1
+				// Display all the files names in ascending order
+				// database.updateFiles(sp);
+				if (database.displayAllFiles().size() == 0)
+					System.out.println("No Files Are Added Yet");
+
+				for (SpecifiedFiles f : database.displayAllFiles()) {
+					System.out.println(f);
+				}
+				System.out.println();
+				break;
+
+			case 2:
+				// Option 2
+				// Nested switch Statement : enter the the matched character to select the
+				// option
+				// Add , Delete and Search => a user specified file or Navigate to the main
+
+				if (choice == 2) {
+					System.out.println(" Select from the Sub menu ");
+					supMenuOptions();
+
+				}
+
+				while (true) {
+
+					supOption = input.nextInt();
+
+					switch (supOption) {
+					case 1:
+						// 1. Add New file to the list
+
+						System.out.println("Enter file name");
+						String fname = input.next();
+						System.out.println("Enter file Directory");
+						String fDirectory = input.next();
+						System.out.println("Enter File id");
+						int fId = input.nextInt();
+						sp = new SpecifiedFiles(fname, fDirectory, fId);
+						database.addFile(sp);
+						System.out.println("File is added successfully");
+						break;
+					case 2:
+						// 2. Search for a file in the list
+
+						break;
+					case 3:
+						// 3. Delete a file in the list
+						break;
+					case 4:
+						// 4. Navigate back to the main context
+						displayMenu();
+						break;
+
+					}
+					break;
+				}
+
+			case 3:
+				// Option 3
+				// Close the application
+				System.out.println("Application is closed");
+				System.exit(choice);
+
+				input.close();
+				break;
+
+			default:
+				break;
 			}
-			System.out.println();
-			break;
-			
-		case 2:
-			//Option 2
-			//Add , Delete and Search => a user specified file or Navigate to the main Context 
-		
-					System.out.println("Adding New file is selected");
-				//Adding files logic
-		
-			     System.out.println("Enter file name");
-                  String fname = input.next();
-                  System.out.println("Enter file Directory");
-                  String fDirectory= input.next() ;
-                  System.out.println("Enter File id");
-                  int fId = input.nextInt();
-                  Calendar cl ;
-                  
-                   
-                  sp = new SpecifiedFiles(fname,fDirectory,fId);
-                  database.addFile(sp);
-                  System.out.println("File is added successfully");
-                  
-                  
-				
-				
-	
-				
-			//1. Add New file to the list
-			//2. Delete a file in the list
-			//3. Search*/
-			break;
-			
-		case 3:
-			//Option 3
-			//Close the application
-			break;
 
-		default:
-			break;
 		}
-
 	}
-  }
 
 }
